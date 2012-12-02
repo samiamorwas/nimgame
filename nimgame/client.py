24de327c6e1b881c775ee7fded6e4d4a27c6ad99
samiamorwas <samiamorwas@gmail.com>
2012-12-01 21:05:57 -0500

    Add dummy client and server.

---
+++ nimgame/client.py
@@ -0,0 +1,43 @@
+import socket
+import sys
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error, msg:
+    print 'Failed to create socket. Error code: '
+    + str(msg[0]) + 'Error message: ' + msg[1]
+    sys.exit()
+
+print 'Socket created'
+
+host = "www.google.com"
+port = 80
+
+try:
+    remote_ip = socket.gethostbyname(host)
+
+except socket.gaierror:
+    print 'Hostname could not be resolved. Exiting.'
+    sys.exit()
+
+print 'IP address of ' + host + ' is ' + remote_ip
+
+s.connect((remote_ip, port))
+
+print 'Socket connected to ' + host + ' on ip ' + remote_ip
+
+message = "GET / HTTP/1.1\r\n\r\n"
+
+try:
+    s.sendall(message)
+except socket.error:
+    print 'Send failed'
+    sys.exit()
+
+print 'Message sent successfully'
+
+reply = s.recv(4096)
+
+print reply
+
+s.close()
